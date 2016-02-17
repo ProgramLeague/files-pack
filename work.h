98c71b7a5914adab0ee1caf449a87d892bf41df7
Mingkai Zhou <524019351@qq.com>
2016-02-17 08:57:58 +0800

    Update work.h

--- work.h
+++ work.h
@@ -5,7 +5,8 @@
 class pack
 {
 public:
-  pack(); //tik
+  pack(TCHAR dir_out[MAX_PATH],CHAR dir_index[MAX_PATH]);//输出文件目录和XML索引目录
+private:
   FILE* fpOut;
   long PackFolder(LPCTSTR szFolderPath, TiXmlElement* parent_node);
   void UnpackFolder(LPCTSTR packPath, LPCTSTR indexPath);
